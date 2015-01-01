e9962a9023c028affa6a77004b07cc9568fb4e95
Marcel Ribeiro Dantas <mribeirodantas@lais.huol.ufrn.br>
2015-01-01 19:00:44 -0300

    Created settings file, plus split install method in setup

---
+++ settings.py
@@ -0,0 +1,4 @@
+import os
+GKT_PATH = os.getenv("HOME") + "/.gkeeptrack/"
+VERSION = '0.1'
+DB_NAME = 'gkt.db'
--- setup.py
+++ setup.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python
 
-import sqlite3
 import os
 import errno
+import sqlite3
+from settings import GKT_PATH, VERSION, DB_NAME
 
 
-def install(gkt_path):
+def install(GKT_PATH):
+    print("Installing GKeepTrack " + VERSION + "...")
     # Create gkeeptrack configuration directory
     try:
-        os.makedirs(gkt_path)
+        os.makedirs(GKT_PATH)
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
     # Change to $HOME/.gkeeptrack/
     try:
-        os.chdir(gkt_path)
+        os.chdir(GKT_PATH)
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
     # Creating projects directory
     try:
-        os.makedirs('projects')
+        os.makedirs("projects")
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
     # Creating data directory
     try:
-        os.makedirs('data')
+        os.makedirs("data")
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
     # Change to $HOME/.gkeeptrack/data/
     try:
-        os.chdir('data')
+        os.chdir("data")
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
-    if os.path.exists('gkt.db'):
+    if os.path.exists(DB_NAME):
         print("There is already a database configured. Override?[y/n]")
         choice = raw_input().lower()
-        if choice == 'n':
+        if choice == "n":
             print("Leaving setup...")
             exit()
 
-    if os.path.exists('gkt.db'):
-        os.remove('gkt.db')
+    if os.path.exists(DB_NAME):
+        os.remove(DB_NAME)
     print("Creating database...")
-    conn = sqlite3.connect('gkt.db')
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
 
     # Create tables
@@ -74,39 +76,43 @@ def install(gkt_path):
     print("Do you want to create a project now?")
     print("You can still do it later. [y/n]")
     choice = raw_input().lower()
-    if choice == 'y':
+    if choice == "y":
         print("What is the name of your project?")
         project_name = raw_input().lower()
-        print("Creating %s...") % project_name
-        # Change to $HOME/.gkeeptrack/projects/
+        add_new_project(project_name)
+    print("Installation is done.")
+
+
+def add_new_project(project_name):
+    print("Creating %s...") % project_name
+    # Change to $HOME/.gkeeptrack/projects/
+    try:
+        os.chdir(GKT_PATH + "/projects")
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    # Isn"t the project already created?
+    if os.path.exists(project_name):
+        print("Project " + project_name + " already exists. Skipping...")
+    else:
+        # Create the project file configuration
         try:
-            os.chdir(gkt_path + '/projects')
+            expression = "touch " + project_name
+            os.system(expression)
         except OSError as exception:
             if exception.errno != errno.EEXIST:
                 raise
-        # Create project file configuration
-        if os.path.exists(project_name):
-            print("Project " + project_name + " already exists. Skipping...")
-        else:
-            try:
-                expression = 'touch ' + project_name
-                os.system(expression)
-            except OSError as exception:
-                if exception.errno != errno.EEXIST:
-                    raise
-    print("Installation is done.")
 
 if __name__ == "__main__":
-    gkt_path = os.getenv("HOME") + "/.gkeeptrack/"
     # Is GKeepTrack already installed?
-    if os.path.isdir(gkt_path):
+    if os.path.isdir(GKT_PATH):
         print("It seems you already have a GKeepTrack installation in")
         print("in your system. Do you still want to proceed? (y/n)")
         choice = raw_input().lower()
-        if choice == 'y':
-            install(gkt_path)
+        if choice == "y":
+            install(GKT_PATH)
         else:
             print("Leaving setup...")
             exit()
     else:
-        install(gkt_path)
+        install(GKT_PATH)
